50776ee89ff81f6f3ad09aaa9bfde9555536a57e
mxerinda <mxerinda@singular.co.za>
2023-06-01 02:49:41 -0700

    Task

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# 5-variable_load.py
+
+from variable_load_5 import a
 
 if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
-    from variable_load_5 import a
-
-    print(a)
+    print("{:d}".format(a))
