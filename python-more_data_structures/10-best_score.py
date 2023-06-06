1802f6457b1cf53ef39430e984da822996446f5b
mxerinda <mxerinda@singular.co.za>
2023-06-06 02:40:41 -0700

    task

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def best_score(my_dict):
-    if my_dict is None or len(my_dict) == 0:
-        return(None)
-    sorted_dict = sorted(my_dict)
-    return(sorted_dict[-1])
+def best_score(a_dictionary):
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
