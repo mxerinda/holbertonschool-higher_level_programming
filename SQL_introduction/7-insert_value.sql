19e65a67e7785a4d75a31f1e346f2aa0255c1b6c
mxerinda <mxerinda@singular.co.za>
2023-07-24 01:05:00 -0700

    Tasks

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,5 @@
---  inserts a new row in the table
-INSERT INTO first_table (id,name) value (89,'Holberton School');
+-- File: 7-insert_value.sql
+
+USE hbtn_0c_0; -- Switch to the appropriate database
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
