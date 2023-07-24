2122e8b16ef20b10d7c61460f90b7a1026debaa8
mxerinda <mxerinda@singular.co.za>
2023-07-24 09:19:59 +0200

    task

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- Inserts a new row in the table `first_table`
--- in database `hbtn_0c_0` in MySQL Server.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School")
+--  inserts a new row in the table
+INSERT INTO first_table (id,name) value (89,'Holberton School');
