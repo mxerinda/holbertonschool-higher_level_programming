a2fd0fe7628982f25ec6e2c0380928e76b1ec47f
mxerinda <mxerinda@singular.co.za>
2023-07-24 02:01:48 -0700

    task

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,5 @@
 -- 1-create_user.sql
--- Create the user if it doesn't exist, and set the password
+-- creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- Grant all privileges to the user
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- Make sure the privileges are applied immediately
 FLUSH PRIVILEGES;
