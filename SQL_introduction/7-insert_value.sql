c7684a07d92973e10c5dfac6e19face8b1c139b5
mxerinda <mxerinda@singular.co.za>
2023-07-24 01:10:02 -0700

    Tasks

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -2,4 +2,4 @@
 
 USE hbtn_0c_0; -- Switch to the appropriate database
 
-INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+INSERT INTO 'first_table' (id, name) VALUES (89, 'Best School');
